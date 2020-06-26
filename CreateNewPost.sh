bf515c595a58d0fdcb44f8a3a8256a9c9ada4b2f
Jason Hand <jasonhand24@gmail.com>
2020-06-25 19:24:08 -0600

    Automating-On-Call-Posts

--- CreateNewPost.sh
+++ CreateNewPost.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-wget
-'https://jhandcdn.blob.core.windows.net/blob/hugo/Automating-On-Call-Posts.md'
+wget 'https://jhandcdn.blob.core.windows.net/blob/hugo/Automating-On-Call-Posts.md'
 
 mv Automating-On-Call-Posts.md content/post/
+echo "You should take a look and see if the file downloaded. If not..."
+echo "type .... wget "
+echo "then paste in "
+echo
+https://jhandcdn.blob.core.windows.net/blob/hugo/Automating-On-Call-Posts.md
 echo "New Markdown found at content/post/Automating-On-Call-Posts.md. Does the
 header look correct (y/n)"
 read Answer
---
+++ content/post/Automating-On-Call-Posts.md
@@ -0,0 +1,18 @@
++++
+title: Automating-On-Call-Posts
+date: 2020-06-26T01:22:16.043Z
+profile: False
+image: 
+placement: 3
+tags: ["jason1tag", "jason2tag"]
+editable: True
++++
+
+## Header One
+This is all of the text that I want to put in under header one.
+## Header Two
+And this is where header two goes.
+For more information go to the next article.
+
+Previous: exploring-the-latency-aspect-of-reliability
+Next: exploring-the-correctness-aspect-of-reliability
