cfd7e40c03eaa735c3252062d33185338b0c6e7d
Jason Hand <jasonhand24@gmail.com>
2020-06-26 12:13:50 -0600

    Cleaning up

--- CreateNewPost.sh
+++ CreateNewPost.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 
-###
-wget 'https://jhand.co/2BFmMty'
+wget -O https://jhandcdn.blob.core.windows.net/blob/hugo/Automating-On-Call-Posts.md
 
-###
+mkdir content/post/Automating-On-Call-Posts
 mv Automating-On-Call-Posts.md content/post/Automating-On-Call-Posts/index.md
-echo "You should take a look and see if the file downloaded. If not..."
-echo "type .... wget "
-echo "then paste in "
-echo
-https://jhandcdn.blob.core.windows.net/blob/hugo/Automating-On-Call-Posts.md
-echo "New Markdown found at content/post/Automating-On-Call-Posts.md. Does the
-header look correct (y/n)"
-read Answer
+
 # Git Steps
-#echo "Sync'ing with repo"
-git add .
+# echo "Pushing to GitHub"
+git add content/post/Automating-On-Call-Posts/index.md
 git commit -m "Automating-On-Call-Posts"
 git push origin master
 echo "######################"
 echo "This has completed"
 echo "#######################"
 echo "Removing script"
-rm CreateNewPost.sh
